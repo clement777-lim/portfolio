fadc0139abc26e37b7f7260fd6594821f97a23ce
Zelts_5555 <78042129+clement777-lim@users.noreply.github.com>
2023-12-24 01:21:09 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
+  github: "https://github.com/clement777-lim",
+  linkedin: "https://www.linkedin.com/in/clement777-lim/",
+  medium: "https://clement777lim.medium.com/",
+  discord: "https://discordapp.com/users/163300027578295808",
 };
 
 export const loaderAnimation = [
